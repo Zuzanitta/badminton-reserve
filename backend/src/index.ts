75172991a9b225d6fe20097cf672aee8df66ab47
Jiahui Lin <susanalin0328@gmail.com>
2026-04-16 21:01:54 +0200

    update google sheet

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -85,7 +85,7 @@ const dayMapping: { [key: string]: string } = {
   'Sunday': '862076829',
 };
 
-const SHEET_BASE_URL = "https://docs.google.com/spreadsheets/d/YOUR_NEW_SPREADSHEET_ID/pub";
+const SHEET_BASE_URL = "https://docs.google.com/spreadsheets/d/1ZXbucUe8oVE8lq4357D9EJYnqcq7z0G1I54dI-L9u3k/pub";
 
 app.get('/api/courts', async (req, res) => {
   try {
